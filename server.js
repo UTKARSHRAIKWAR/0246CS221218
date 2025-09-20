6a52e29204c3042e044d559097bba31eb6e28109
UTKARSHRAIKWAR <utkarshraikwar555@gmail.com>
2025-09-20 13:00:43 +0530

    auth

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import errorHandler from "./middlewares/errorHandler.js";
 import userRoutes from "./routes/user.routes.js";
+import db from "./utils/db.js";
 
 dotenv.config();
 
 const app = express();
+
+db();
 
 app.use(express.json());
 
